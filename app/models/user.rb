55e83223201c24d7084d6c71f733bab780b4a8a8
y-keysynth <ykhojojp@gmail.com>
2019-09-01 20:40:13 +0900

    just_in_case_save-next_flashmessage

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,4 @@ class User < ApplicationRecord
   has_many :messages
   has_many :group_users
   has_many :groups, through: :group_users
-  has_many :messages
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -46,5 +46,5 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   process resize_to_fit: [800, 800]
-  
+
 end
