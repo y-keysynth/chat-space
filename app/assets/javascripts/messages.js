72ae3179c32a70e9e648e53a472ba4ebf1627fed
y-keysynth <ykhojojp@gmail.com>
2019-09-05 20:54:22 +0900

    push-#1_to_LGTM

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,44 +1,49 @@
-$(function(){
-  function buildMessage(message) {
-    var html = `<div class="message">
-                  <div class="message__upper-info">
-                  <p class="message__upper-info__talker">
-                  ${message.name}
-                  </p>
-                  <p class="message__upper-info__date">
-                  ${message.created_at}
-                  </p>
-                  </div>
-                  <p class="message__text">
-                  </p><p class="lower-message__content">
-                  ${message.content}
-                  </p>
-                  <p></p>
-                </div>`
-    return html;
-  }
+$(document).on('turbolinks:load', function(){
+  $(function(){
+    function buildMessage(message) {
+      var imagejudg = (message.image.url === null) ? '' : `<img src="${message.image.url}">`;
+      var html = `<div class="message">
+                    <div class="message__upper-info">
+                    <p class="message__upper-info__talker">
+                    ${message.name}
+                    </p>
+                    <p class="message__upper-info__date">
+                    ${message.created_at}
+                    </p>
+                    </div>
+                    <p class="message__text">
+                    ${message.content}
+                    </p><p class="lower-message__content">
+                    ${imagejudg}
+                    </p>
+                    <p></p>
+                  </div>`
+      return html;
+    }
 
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false,
+    $('#new_message').on('submit', function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false,
+      })
+      .done(function(message){
+        var html = buildMessage(message);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        $('.messages').append(html);
+        $('#message_content').val('');
+        $('.form__submit').removeAttr('disabled');
+      })
+      .fail(function(){
+        alert('メッセージを入力してください');
+        $('.form__submit').removeAttr('disabled');
+      })
     })
-    .done(function(message){
-      var html = buildMessage(message);
-      $('.messages').append(html);
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-      $('#message_content').val('');
-      $('.form__submit').removeAttr('disabled');
-    })
-    .fail(function(){
-      alert('メッセージを入力してください');
-    })
-  })
+  });
 });
