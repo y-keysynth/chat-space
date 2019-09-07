7ae93179e6203c4c86d8a52b38c0f4afd8c62b7e
y-keysynth <ykhojojp@gmail.com>
2019-09-07 20:56:40 +0900

    三項演算子に変換する

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -47,4 +47,88 @@ $(document).on('turbolinks:load', function(){
       })
     })
   });
+  
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: '/groups/${id:last}/api/messages',
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      // var rul = api/messages#index {:format=>"json"} して↑のurl: urlがいいかも？
+      
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+
+      //メッセージが入ったHTMLを取得
+
+      //メッセージを追加
+
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+
+  var buildMessageHTML = function(message) {
+    if (message.content && message.image.url) {
+      //data-idが反映されるようにしている
+      var html = `<div class="message" data-message-id="${message.id}"> 
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="upper-message__date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="lower-meesage">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                      ${message.image}
+                    </div>
+                  </div>`
+    } else if (message.content) {
+      //同様に、data-idが反映されるようにしている
+      var html = `<div class="message" data-message-id="${message.id}"> 
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="upper-message__date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="lower-meesage">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                    </div>
+                  </div>`
+    } else if (message.image.url) {
+      //同様に、data-idが反映されるようにしている
+      var html = `<div class="message" data-message-id="${message.id}"> 
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="upper-message__date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="lower-meesage">
+                      ${message.image}
+                    </div>
+                  </div>`
+    };
+    return html;
+  };
 });
